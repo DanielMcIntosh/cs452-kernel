e3a49ff1df15d7b7cc32a3b70a0eb0597fdff911
Brian Allan Forbes <baforbes@uwaterloo.ca>
2018-07-07 00:46:56 -0400

    func command

--- src/command.c
+++ src/command.c
@@ -423,6 +423,14 @@ void task_commandserver(int trackstate_tid, int trainstate_tid){
             NotifyReservation(trainstate_tid, cm.command.arg1);
             break;
         }
+        case COMMAND_FUNC:
+        {
+            int train = cm.command.arg1;
+            int fn = cm.command.arg2;
+            Putc(servertid, 1, 64 + fn);
+            Putc(servertid, 1, train);
+            break;
+        }
         case COMMAND_QUIT:
         {
             Quit();
